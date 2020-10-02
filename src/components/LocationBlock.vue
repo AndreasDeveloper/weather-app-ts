16aa46b4284419c48f78c72fc4bd71ea3ad06e13
unknown <andreasmrdja16@gmail.com>
2020-10-02 17:50:36 +0200

    Created interface for the fetched objects. Formatted date.

--- src/components/LocationBlock.vue
+++ src/components/LocationBlock.vue
@@ -1,8 +1,13 @@
 <template>
-    <div>
+    <div v-if="location.location">
         <h2 class="heading-divider">Result</h2>
         <div class="location-block">
-            {{ location }}
+            <div class="location-block__content">
+                <h2>{{ location.location.name }}</h2>
+                <h3>{{ location.location.country }}</h3>
+                <span>{{ date }}.</span>
+                <span>{{ time }}</span>
+            </div>
         </div>
     </div>
 </template>
@@ -10,26 +15,43 @@
 <script lang="ts">
 import Vue from 'vue';
 
+// Intefrace for fetched data objects
+interface LocationInter {
+    data: {
+        current: object;
+        location: {
+            localtime: string;
+        };
+    };
+}
+
 export default Vue.extend({
     name: 'LocationBlock',
     props: {
-        locationData: { type: Object }
+        locationData: { type: Object as () => LocationInter }
     },
     watch: {
         locationData: {
             deep: true,
-            handler(val, old) {
-                console.log(val, old)
+            handler(val) {
                 this.location = val.data;
+                this.formatDate(val);
             }
         }
     },
     data() {
         return {
-            location: {}
+            location: {},
+            date: '',
+            time: ''
         };
     },
     methods: {
+        formatDate(val: LocationInter) {
+            const newDate = val.data.location.localtime.split('-').join('.').split(' ');
+            this.date = newDate[0];
+            this.time = newDate[1];
+        }
     }
 })
 </script>
