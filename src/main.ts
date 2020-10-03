9bfe7a6f852abf2f7433647086f3f53b54336b4d
unknown <andreasmrdja16@gmail.com>
2020-10-03 15:22:13 +0200

    Imported vue-meta plugin to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import VueMeta from 'vue-meta';
 
-Vue.config.productionTip = false
+Vue.use(VueMeta);
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
